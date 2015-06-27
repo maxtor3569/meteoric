9da84a13c915ad0c372ef8e749c476b9e02d7484
Maxime <maxtor3569@gmail.com>
2015-06-28 00:24:55 +0200

    Add mobile config

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+App.info({
+  id: 'com.teamsport.app',
+  name: 'Teamsport',
+  description: 'Teamsport',
+  author: 'Maxime Gastaldo',
+  email: 'maxtor3569@gmail.com',
+  website: 'http://teamsport.vn'
+});
+App.accessRule('*.google.com/*');
+App.accessRule('*.googleapis.com/*');
+App.accessRule('*.gstatic.com/*');
